a14c9726c29c04028cabd6e655090884a198c44e
Alex Forsythe <awforsythe@gmail.com>
2022-02-19 00:03:33 -0600

    Implement custom painting in WorkArea

--- app/gui/workarea.py
+++ app/gui/workarea.py
@@ -1,12 +1,17 @@
-from PySide6.QtWidgets import QLabel, QSizePolicy
-from PySide6.QtGui import QImage
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QSizePolicy
+from PySide6.QtGui import QImage, QPainter, QBrush, QColor
 
 
-class WorkArea(QLabel):
+class WorkArea(QWidget):
 
     def __init__(self):
         super().__init__()
-
-        self.image = QImage()
-
         self.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
+
+    def resizeEvent(self, event):
+        pass
+
+    def paintEvent(self, event):
+        p = QPainter(self)
+        p.fillRect(self.rect(), QBrush(QColor(192, 208, 222), Qt.DiagCrossPattern))
